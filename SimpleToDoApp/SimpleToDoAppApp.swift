2b9616c0fdd52b9697122cc390fe09e6fdd1f9f2
soh92 <pix4soh@gmail.com>
2021-12-12 20:15:54 +0900

    Initial Commit

---
+++ SimpleToDoApp/SimpleToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDoAppApp.swift
+//  SimpleToDoApp
+//
+//  Created by sho yoneda on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct SimpleToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
